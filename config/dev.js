d1dc6083fe2bba0b27bbaa22b7d761bebac0b490
HENRY TEICHEIRA <hteicheira@gmail.com>
2018-10-12 11:39:33 -0700

    Initialized firebase and modified firebase config

--- config/dev.js
+++ config/dev.js
@@ -1,8 +1,3 @@
-export const FirebaseConfig = {
-  apiKey: 'AIzaSyA2pQuh4UKAWKY-Oz2NH38goYShIhv4d88',
-  authDomain: 'shoplemarche-f8f4a.firebaseapp.com',
-  databaseURL: 'https://shoplemarche-f8f4a.firebaseio.com',
-  projectId: 'shoplemarche-f8f4a',
-  storageBucket: '',
-  messagingSenderId: '725042631830'
-};
+const functions = require('firebase-functions');
+
+export const FirebaseConfig = functions.config();
